575358edafc5270df22b83f6850be36594742196
Timo Hillmann <timo@clipchamp.com>
2019-01-12 11:12:17 +1000

    feat: add concurrent mode

--- apps/my-app/src/index.tsx
+++ apps/my-app/src/index.tsx
@@ -1,11 +1,21 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+// Run react in concurrent mode
+ReactDOM.unstable_createRoot(document.getElementById(
+  "root"
+) as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <React.StrictMode><App /></React.StrictMode>,
+//   document.getElementById('root') as HTMLElement
+// );
+
 registerServiceWorker();
---
+++ apps/my-app/unstable.d.ts
@@ -0,0 +1,12 @@
+import { Renderer } from "react-dom";
+import { DOMAttributes, DOMElement, ReactElement, Component, ComponentState } from "react";
+
+import * as ReactDOM from "react-dom";
+
+declare module "react-dom" {
+  export function unstable_createRoot<P>(
+    domElement: Element | null
+  ): {
+    render(element: ReactElement<P>): Component<P, ComponentState> | Element | void;
+  };
+}
