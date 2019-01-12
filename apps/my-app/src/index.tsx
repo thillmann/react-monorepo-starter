e70a01998395dcea5b6b552b19ad85c51c91893a
Timo Hillmann <timo@clipchamp.com>
2019-01-12 11:32:12 +1000

    feat: added prettier and applied formatting

--- apps/my-app/src/index.tsx
+++ apps/my-app/src/index.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 // Run react in concurrent mode
 ReactDOM.unstable_createRoot(document.getElementById(
-  "root"
+  'root'
 ) as HTMLElement).render(
   <React.StrictMode>
     <App />
--- apps/my-app/unstable.d.ts
+++ apps/my-app/unstable.d.ts
@@ -1,12 +1,20 @@
-import { Renderer } from "react-dom";
-import { DOMAttributes, DOMElement, ReactElement, Component, ComponentState } from "react";
+import { Renderer } from 'react-dom';
+import {
+  DOMAttributes,
+  DOMElement,
+  ReactElement,
+  Component,
+  ComponentState
+} from 'react';
 
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from 'react-dom';
 
-declare module "react-dom" {
+declare module 'react-dom' {
   export function unstable_createRoot<P>(
     domElement: Element | null
   ): {
-    render(element: ReactElement<P>): Component<P, ComponentState> | Element | void;
+    render(
+      element: ReactElement<P>
+    ): Component<P, ComponentState> | Element | void;
   };
 }
